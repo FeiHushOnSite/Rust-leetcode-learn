224e1f191e94175ce8dfe9906a7985f59b61d195
Jerry Ji <jerry.ji@boehringer-ingelheim.com>
2024-10-10 10:12:35 +0800

    fix: cargo config and some file lost

--- src/tests/test_satisfies_conditions.rs
+++ src/tests/test_satisfies_conditions.rs
@@ -1,11 +1,11 @@
-use crate::TestFuc;
+// use crate::TestFuc;
 
 #[test]
 pub fn test_satisfies_conditions() {
-    let dept = Dog;
-    dept.test_fuc();
-    print_sound(&dept);
-    println!("{:?}", Department::test_fuc(&Department{ dep_id: 10, dep_name: "bix".to_string()}));
+    // let dept = Dog;
+    // dept.test_fuc();
+    // print_sound(&dept);
+    // println!("{:?}", Department::test_fuc(&Department{ dep_id: 10, dep_name: "bix".to_string()}));
     // let grid: [[i32; 3]; 2] = [[1,0,2],[1,0,2]];
     let grid: Vec<Vec<i32>> = vec![vec![1], vec![2], vec![3]];
     let res = satisfies_conditions(grid);
@@ -17,29 +17,29 @@ pub fn test_satisfies_conditions() {
 
 struct Dog;
 
-impl TestFuc for Dog {
-    fn test_fuc(&self) {
-        println!("Woof!")
-    }
-}
-
-fn print_sound(a: &dyn TestFuc) {
-    a.test_fuc();
-}
+// impl TestFuc for Dog {
+//     fn test_fuc(&self) {
+//         println!("Woof!")
+//     }
+// }
+//
+// fn print_sound(a: &dyn TestFuc) {
+//     a.test_fuc();
+// }
 
 struct Department {
     dep_id: u8,
     dep_name: String
 }
 
-impl TestFuc for Department {
-    fn test_fuc(&self) {
-        println!("department id :{}", self.dep_id);
-        println!("department name :{}", self.dep_name);
-        println!("This is the implementation for my_method in MyStruct");
-        // todo!();
-    }
-}
+// impl TestFuc for Department {
+//     fn test_fuc(&self) {
+//         println!("department id :{}", self.dep_id);
+//         println!("department name :{}", self.dep_name);
+//         println!("This is the implementation for my_method in MyStruct");
+//         // todo!();
+//     }
+// }
 
 pub fn satisfies_conditions(grid: Vec<Vec<i32>>) -> bool {
     let m = grid.len();
